27d580670186b7aeafd44a6645e2295c1c89550f
YOKOTA, Takehiko <skirnir@gmail.com>
2008-09-18 09:03:04 +0000

    インデックスが張られておらずパフォーマンスの問題が発生したためインデックスを張るようにした。
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/package/trunk@2073 ccb6d941-20fb-0310-8948-d27bf48cceb9

--- plugin/org.seasar.kvasir.page.ability.content/src/main/resources/org/seasar/kvasir/page/ability/content/dao/ContentDao.sql
+++ plugin/org.seasar.kvasir.page.ability.content/src/main/resources/org/seasar/kvasir/page/ability/content/dao/ContentDao.sql
@@ -1,9 +1,12 @@
 #
 # FOR INITIALIZATION
 #
-initialize=createTable
+initialize=createTable,createIndex1
 initialize.createTable= \
     CREATE TABLE content ( \
         pageid              INTEGER NOT NULL, \
         modifydate          TIMESTAMP NOT NULL \
     )
+initialize.createIndex1= \
+    CREATE INDEX content_pageid_idx ON content (pageid)
+    

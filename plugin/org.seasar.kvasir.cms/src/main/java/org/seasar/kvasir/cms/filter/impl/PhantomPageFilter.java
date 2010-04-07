56db883ed4b022bf761d54391c4e97ed14d087dd
YOKOTA, Takehiko <skirnir@gmail.com>
2010-04-07 08:55:11 +0000

    Javadocを追記した。
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/package/trunk@2183 ccb6d941-20fb-0310-8948-d27bf48cceb9

--- plugin/org.seasar.kvasir.cms/src/main/java/org/seasar/kvasir/cms/filter/impl/PhantomPageFilter.java
+++ plugin/org.seasar.kvasir.cms/src/main/java/org/seasar/kvasir/cms/filter/impl/PhantomPageFilter.java
@@ -15,6 +15,20 @@ import org.seasar.kvasir.webapp.Dispatcher;
 
 
 /**
+ * 常にPageNotFoundRuntimeExceptionをスローするFilterです。
+ * <p>あるパスへの直接のアクセスを許可したくない場合、このFilterを使うことができます。
+ * 例えばpageFilters拡張ポイントに以下のエントリをプラグインすることで
+ * 「*.html」というパスへの直接アクセスを禁止することができます：
+ * </p>
+ * <pre>
+ * &lt;page-filter id="phantomPageFilter"
+ *   phase="initialization" what="path" how="^.+\.html$"
+ *   regex="true" /&gt;
+ * </pre>
+ * <p>（このクラスのインスタンスがphantomPageFilterという名前で
+ * org.seasar.kvasir.cmsプラグインのplugin.diconに登録されているので、
+ * <code>id="phantomPageFilter"</code>と指定した場合はコンポーネントの登録は不要です。）
+ * </p>
  * <p><b>同期化：</b>
  * このクラスはスレッドセーフです。
  * </p>

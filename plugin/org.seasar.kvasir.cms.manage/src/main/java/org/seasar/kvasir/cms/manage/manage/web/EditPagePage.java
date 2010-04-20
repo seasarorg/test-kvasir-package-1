18727631b421cdde29e16b600db332f4a088a849
YOKOTA, Takehiko <skirnir@gmail.com>
2010-04-20 08:06:06 +0000

    管理ツールが対応していないタイプのページの編集画面を開くとnot foundになってしまう問題点に対処した。
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/package/trunk@2203 ccb6d941-20fb-0310-8948-d27bf48cceb9

--- plugin/org.seasar.kvasir.cms.manage/src/main/java/org/seasar/kvasir/cms/manage/manage/web/EditPagePage.java
+++ plugin/org.seasar.kvasir.cms.manage/src/main/java/org/seasar/kvasir/cms/manage/manage/web/EditPagePage.java
@@ -1,25 +1,38 @@
 package org.seasar.kvasir.cms.manage.manage.web;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+
+import net.skirnir.freyja.render.Note;
+import net.skirnir.freyja.render.Notes;
+import net.skirnir.freyja.render.html.OptionTag;
+
 import org.seasar.cms.ymir.Request;
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
 import org.seasar.kvasir.cms.manage.PageService;
 import org.seasar.kvasir.cms.manage.tab.impl.PageTab;
 import org.seasar.kvasir.page.Page;
 import org.seasar.kvasir.page.PathId;
 import org.seasar.kvasir.page.ability.PropertyAbility;
 import org.seasar.kvasir.page.condition.PageCondition;
+import org.seasar.kvasir.page.type.GenericPageType;
 import org.seasar.kvasir.page.type.PageType;
 import org.seasar.kvasir.page.type.User;
 
-import net.skirnir.freyja.render.Note;
-import net.skirnir.freyja.render.Notes;
-import net.skirnir.freyja.render.html.OptionTag;
 
-public class EditPagePage extends MainPanePage {
+public class EditPagePage extends MainPanePage
+{
     private OptionTag[] ownerUserCandidates_;
+
+    @Binding(bindingType = BindingType.MUST)
+    protected ServletContext servletContext;
+
 
     /*
      * set by framework
@@ -34,13 +47,14 @@ public class EditPagePage extends MainPanePage {
      */
 
     @SuppressWarnings("unchecked")
-    public Object do_execute() {
+    public Object do_execute()
+    {
         Page page = getPage();
         PageType pageType = getPagePlugin().getPageType(page.getType());
 
         if (Request.METHOD_POST.equalsIgnoreCase(getYmirRequest().getMethod())) {
             Notes notes = getPageService().updatePage(page,
-                    getYmirRequest().getParameterMap());
+                getYmirRequest().getParameterMap());
             if (notes == null) {
                 page.touch();
                 setNotes(new Notes().add(new Note("app.note.editPage.succeed")));
@@ -54,38 +68,52 @@ public class EditPagePage extends MainPanePage {
         return render(pageType);
     }
 
+
     /*
      * private scope methods
      */
 
-    private String render(PageType pageType) {
+    private String render(PageType pageType)
+    {
         prepareOwnerUser();
         enableTab(PageTab.NAME_EDIT);
         enableLocationBar(true);
-        return "/" + pageType.getId() + "/edit-page.html";
+        String path = "/" + pageType.getId() + "/edit-page.html";
+        URL url;
+        try {
+            url = servletContext.getResource(path);
+        } catch (MalformedURLException ex) {
+            throw new RuntimeException("Logic error", ex);
+        }
+        if (url == null) {
+            path = "/" + GenericPageType.ID + "/edit-page.html";
+        }
+        return path;
     }
 
-    void prepareOwnerUser() {
+
+    void prepareOwnerUser()
+    {
         List<OptionTag> ownerUserCandidateList = new ArrayList<OptionTag>();
 
         User ownerUser = getPage().getOwnerUser();
         int ownerUserId = PageService.USERID_UNKNOWN;
         if (ownerUser == null) {
             ownerUserCandidateList.add(new OptionTag(String
-                    .valueOf(PageService.USERID_UNKNOWN),
-                    getResource("app.label.unknown")).setSelected(true));
+                .valueOf(PageService.USERID_UNKNOWN),
+                getResource("app.label.unknown")).setSelected(true));
         } else {
             ownerUserId = ownerUser.getId();
         }
 
         int heimId = getCurrentHeimId();
-        User[] users = (User[]) getPageAlfr().getPages(heimId,
-                new PageCondition().setType(User.TYPE));
+        User[] users = (User[])getPageAlfr().getPages(heimId,
+            new PageCondition().setType(User.TYPE));
         if (heimId != PathId.HEIM_MIDGARD) {
             List<User> userList = new ArrayList<User>();
-            userList.addAll(Arrays.asList((User[]) getPageAlfr().getPage(
-                    Page.ID_USERS).getChildren(
-                    new PageCondition().setType(User.TYPE))));
+            userList.addAll(Arrays.asList((User[])getPageAlfr().getPage(
+                Page.ID_USERS).getChildren(
+                new PageCondition().setType(User.TYPE))));
             userList.addAll(Arrays.asList(users));
             users = userList.toArray(new User[0]);
         }
@@ -93,9 +121,9 @@ public class EditPagePage extends MainPanePage {
         for (int i = 0; i < users.length; i++) {
             StringBuilder sb = new StringBuilder();
             String label = users[i].getAbility(PropertyAbility.class)
-                    .getProperty(PropertyAbility.PROP_LABEL, getLocale());
+                .getProperty(PropertyAbility.PROP_LABEL, getLocale());
             if (heimId != PathId.HEIM_MIDGARD
-                    && users[i].getHeimId() == PathId.HEIM_MIDGARD) {
+                && users[i].getHeimId() == PathId.HEIM_MIDGARD) {
                 sb.append('/');
             }
             sb.append(users[i].getPathname()).append(" (");
@@ -104,17 +132,19 @@ public class EditPagePage extends MainPanePage {
             }
             sb.append(')');
             ownerUserCandidateList.add(new OptionTag(String.valueOf(users[i]
-                    .getId()), sb.toString())
-                    .setSelected(users[i].getId() == ownerUserId));
+                .getId()), sb.toString())
+                .setSelected(users[i].getId() == ownerUserId));
         }
         ownerUserCandidates_ = ownerUserCandidateList.toArray(new OptionTag[0]);
     }
+
 
     /*
      * for framework / presentation tier
      */
 
-    public OptionTag[] getOwnerUserCandidates() {
+    public OptionTag[] getOwnerUserCandidates()
+    {
         return ownerUserCandidates_;
     }
 }

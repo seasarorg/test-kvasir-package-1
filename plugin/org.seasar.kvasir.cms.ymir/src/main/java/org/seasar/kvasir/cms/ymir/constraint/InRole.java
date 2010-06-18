e178ba5a226cab7e84205216df8c24ac640fe0cc
YOKOTA, Takehiko <skirnir@gmail.com>
2010-06-18 14:21:02 +0000

    Javadocコメントを追記。
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/package/trunk@2293 ccb6d941-20fb-0310-8948-d27bf48cceb9

--- plugin/org.seasar.kvasir.cms.ymir/src/main/java/org/seasar/kvasir/cms/ymir/constraint/InRole.java
+++ plugin/org.seasar.kvasir.cms.ymir/src/main/java/org/seasar/kvasir/cms/ymir/constraint/InRole.java
@@ -9,10 +9,34 @@ import org.seasar.cms.ymir.extension.ConstraintType;
 import org.seasar.cms.ymir.extension.annotation.ConstraintAnnotation;
 
 
+/**
+ * 現在のユーザが指定されたロールを持つ必要があることを表わすアノテーションです。
+ * 
+ * @author skirnir
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target( { ElementType.TYPE, ElementType.METHOD })
 @ConstraintAnnotation(type = ConstraintType.PERMISSION, factory = InRoleConstraintFactory.class)
 public @interface InRole
 {
+    /**
+     * ロールのパス名です。
+     * <p>パス名としては絶対パスと相対パスを指定することができます。
+     * </p>
+     * <p>絶対パスは「<code>/</code>」で始まるパスで、
+     * リクエストされたHeim上の絶対パスの位置に存在するロールを表わします。
+     * </p>
+     * <p>相対パスは「<code>@/</code>」で始まるパスで、
+     * リクエストされたページ（存在しない場合はリクエストパスの直近のページ）
+     * の属するLordからの相対パス位置に存在するロールを表わします。
+     * </p>
+     * <p>パスで指定されたロールが存在しない場合には実行時に
+     * {@link IllegalArgumentException}がスローされます。
+     * </p>
+     * <p>複数のパスを指定した場合は、いずれかのロールに属していることを表わします。
+     * </p>
+     * 
+     * @return ロールのパス名。
+     */
     String[] value() default {};
 }

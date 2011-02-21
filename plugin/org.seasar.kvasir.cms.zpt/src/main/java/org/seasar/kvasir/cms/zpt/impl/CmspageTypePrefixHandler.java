da1863f0842fb9a333a3d0c9b7f603ba57ad190b
YOKOTA, Takehiko <skirnir@gmail.com>
2011-02-21 13:20:26 +0000

    lord相対パスの解釈やパス中の${...}の評価をするようにした。
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/package/trunk@2323 ccb6d941-20fb-0310-8948-d27bf48cceb9

--- plugin/org.seasar.kvasir.cms.zpt/src/main/java/org/seasar/kvasir/cms/zpt/impl/CmspageTypePrefixHandler.java
+++ plugin/org.seasar.kvasir.cms.zpt/src/main/java/org/seasar/kvasir/cms/zpt/impl/CmspageTypePrefixHandler.java
@@ -5,13 +5,17 @@ import javax.servlet.http.HttpServletRequest;
 import net.skirnir.freyja.TemplateContext;
 import net.skirnir.freyja.VariableResolver;
 import net.skirnir.freyja.zpt.tales.TalesExpressionEvaluator;
+import net.skirnir.freyja.zpt.tales.TalesUtils;
 import net.skirnir.freyja.zpt.tales.TypePrefixHandler;
 import net.skirnir.freyja.zpt.webapp.ServletTalesExpressionEvaluator;
 
 import org.seasar.kvasir.base.Asgard;
 import org.seasar.kvasir.base.plugin.PluginAlfr;
 import org.seasar.kvasir.cms.CmsPlugin;
+import org.seasar.kvasir.cms.PageDispatch;
+import org.seasar.kvasir.page.Page;
 import org.seasar.kvasir.page.PagePlugin;
+import org.seasar.kvasir.page.PageUtils;
 
 
 /**
@@ -43,22 +47,54 @@ public class CmspageTypePrefixHandler
         PluginAlfr pluginAlfr = Asgard.getKvasir().getPluginAlfr();
         HttpServletRequest request = evaluator_.getRequest(varResolver);
 
-        Integer heimId = null;
+        String heimPart = null;
         String pathname = expr;
         int colon = expr.indexOf(':');
         if (colon >= 0) {
-            try {
-                heimId = Integer.valueOf(expr.substring(0, colon));
-                pathname = expr.substring(colon + 1);
-            } catch (Throwable ignore) {
-            }
+            heimPart = expr.substring(0, colon);
+            pathname = expr.substring(colon + 1);
         }
-        if (heimId == null) {
+
+        heimPart = TalesUtils.resolveAsString(context, heimPart, varResolver,
+            evaluator_);
+        pathname = toAbsolutePathname(context, varResolver, TalesUtils
+            .resolveAsString(context, pathname, varResolver, evaluator_));
+
+        int heimId;
+        if (heimPart != null) {
+            heimId = Integer.parseInt(heimPart);
+        } else {
             heimId = pluginAlfr.getPlugin(CmsPlugin.class).determineHeimId(
                 request);
         }
 
         return pluginAlfr.getPlugin(PagePlugin.class).getPageAlfr().getPage(
             heimId, pathname);
+    }
+
+
+    String toAbsolutePathname(TemplateContext context,
+        VariableResolver varResolver, String pathname)
+    {
+        if (pathname == null) {
+            return null;
+        }
+
+        String lordPathname = null;
+        if (pathname.startsWith(KvasirVariableResolver.VARNAME_THATLORD)) {
+            lordPathname = ((Page)context.getVariableResolver().getVariable(
+                context, KvasirVariableResolver.VARNAME_THATLORD))
+                .getPathname();
+        } else if (pathname.startsWith(KvasirVariableResolver.VARNAME_MYLORD)) {
+            lordPathname = ((Page)context.getVariableResolver().getVariable(
+                context, KvasirVariableResolver.VARNAME_MYLORD)).getPathname();
+        }
+
+        String basePathname = ((PageDispatch)context.getVariableResolver()
+            .getVariable(context, KvasirVariableResolver.VARNAME_THAT))
+            .getPathname();
+
+        return PageUtils.getAbsolutePathname(pathname, lordPathname,
+            basePathname);
     }
 }
